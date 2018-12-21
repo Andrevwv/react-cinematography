44608dddec4cda52ed35f4b5aa1ac5fab49dfa3f
andrevwv <andrevwv@gmail.com>
2018-12-21 17:53:37 +0200

    added no-poster image

--- src/components/PreviewItem/index.js
+++ src/components/PreviewItem/index.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import './PreviewItem.scss'
+import noPoster from './no-poster.jpg'
 
 class PreviewItem extends Component {
     render() {
+        const imageSrc = this.props.object.poster_path !== null 
+            ? `${this.props.settings.images.base_url}${this.props.settings.images.poster_sizes[1]}${this.props.object.poster_path}` 
+            : noPoster;
+        console.log(this.props.object.poster_path);
         return (
             <div className="previev-item">
                 <img className="image" 
-                    src={`${this.props.settings.images.base_url}${this.props.settings.images.poster_sizes[1]}${this.props.object.poster_path}`} 
+                    src={imageSrc} 
                     alt={`Poster for ${this.props.object.title}`}
                     >
                 </img>
